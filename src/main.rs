cd895c7c55bb1d82b4e7057917e420e0ffc2e3bc
robert <r_girard@live.com>
2024-04-24 21:56:12 -0400

    sine function

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,19 @@
+fn create_sin(start : f64, stop : f64, steps : u32) -> Vec<f64> {
+    let mut list : Vec<_>  = Vec::new();
+    let step_size : f64 = (stop - start)/(steps as f64);
+    for i in 0..steps {
+        list.push(start + (i as f64)*step_size);
+    }
+    let list : Vec<_> = list.iter()
+                        .map(|&x| (x as f64)*2.0*std::f64::consts::PI)
+                        .map(|x| x.sin())
+                        .collect();
+    list
+}
+
+
 fn main() {
+    let list = create_sin(0.0, 2.0, 30);
     println!("Hello, world!");
+    println!("{:?}", list);
 }

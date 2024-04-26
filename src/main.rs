cd59ce9950ee9ccf64a47b16d89c6c487c537674
robert <r_girard@live.com>
2024-04-25 20:09:15 -0400

    Range iterator

--- src/main.rs
+++ src/main.rs
@@ -11,9 +11,43 @@ fn create_sin(start : f64, stop : f64, steps : u32) -> Vec<f64> {
     list
 }
 
+struct Range {
+    start : f64,
+    end : f64,
+    steps : u32,
+    current : f64,
+}
+
+impl Range {
+    fn new(start : f64, end : f64, steps : u32) -> Range {
+        Range {start:start, end:end, steps:steps, current:start - (end-start)/(steps as f64)}
+    }
+}
+
+impl Iterator for Range {
+    type Item = f64;
+    
+    fn next(&mut self) -> Option<Self::Item> {
+        self.current += (self.end - self.start)/(self.steps as f64);
+        if self.current >= self.end{
+            None
+        } else {
+            Some(self.current)
+        }
+    }
+}
+
+
+// fn rdp_alg(data : Vec<f64>, eps : f64) -> Vec<f64> {
+
+// }
+
 
 fn main() {
     let list = create_sin(0.0, 2.0, 30);
     println!("Hello, world!");
     println!("{:?}", list);
+    let range = Range::new(0.0,10.0,10);
+    let vals : Vec<_> = range.collect();
+    println!("{:?}", vals);
 }

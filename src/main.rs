a15dae32e7f28f9f93a08981bd0e9c491468b905
robert <r_girard@live.com>
2024-04-29 21:14:41 -0400

    drag values

--- src/main.rs
+++ src/main.rs
@@ -1,133 +1,25 @@
 use eframe::egui::{self, DragValue, Event, Vec2};
-use egui_plot::{Line, Legend, PlotPoints, PlotPoint};
-use std::f64::consts::PI;
-use std::env;
+use egui_plot::{Line, Legend, PlotPoints};
 
-fn create_sin(start : f64, stop : f64, steps : u32) -> Vec<Point> {
-    let mut list : Vec<_>  = Vec::new();
-    let step_size : f64 = (stop - start)/((steps -1) as f64);
-    for i in 0..(steps) {
-        list.push(start + (i as f64)*step_size);
-    }
-    let list : Vec<_> = list.iter()
-                        .map(|&x| Point {y: ((x as f64)*2.0*PI).sin(), x})
-                        .collect();
-    list
+mod rdp;
+
+enum MathFunc {
+    Sin,
+    Exp,
 }
 
-#[derive(Debug, Clone, Copy)]
-struct Point {
-    y : f64,
-    x : f64
-}
-
-impl Point {
-    fn as_plot_point(&self) -> PlotPoint {
-        return PlotPoint::new(self.x, self.y);
-    }
-    fn as_arr(&self) -> [f64;2] {
-        return [(&self.x).to_owned(),(&self.y).to_owned()];
-    }
-}
-
-struct Range {
-    start : f64,
-    end : f64,
-    steps : u32,
-    current : f64,
-}
-
-impl Range {
-    fn new(start : f64, end : f64, steps : u32) -> Range {
-        Range {start:start, end:end, steps:steps, current:start - (end-start)/(steps as f64)}
-    }
-}
-
-impl Iterator for Range {
-    type Item = f64;
-    
-    fn next(&mut self) -> Option<Self::Item> {
-        self.current += (self.end - self.start)/(self.steps as f64);
-        if self.current >= self.end{
-            None
-        } else {
-            Some(self.current)
-        }
-    }
-}
-
-
-// struct regularIntervalpoint
-
-fn perpendicular_distance(l1 : &Point, l2 : &Point, p : &Point) -> f64{
-    let num = ((l2.x-l1.x)*(p.y-l1.y) - (p.x - l1.x)*(l2.y-l1.y)).abs();
-    let den = ((l2.x-l1.x).powi(2) + (l2.y-l1.y).powi(2)).sqrt();
-    num/den
-}
-
-fn vertical_distance(l1 : &Point, l2 : &Point, p : &Point) -> f64{
-    let slope = (l2.y-l1.y)/(l2.x-l1.x);
-    let x_dist = p.x-l1.x;
-    let line_y = l1.y + x_dist*slope;
-    (line_y - p.y).abs()
-}
-
-fn rdp_alg(data : &mut Vec<Point>, eps : f64) -> Option<Vec<Point>> {
-    let mut dmax : f64 = 0.0;
-    let mut imax =  0;
-    let l1 = data.first()?;
-    let l2 = data.last()?;
-    for i in 1..(data.len()-2) {
-        let p = data.get(i)?;
-        let d = vertical_distance(l1, l2, p);
-        if d > dmax {
-            imax = i;
-            dmax = d;
-        }
-    }
-    if dmax > eps {
-        let mut first_half = data.clone();
-        let mut second_half = first_half.split_off(imax);
-        first_half.push(second_half.first().unwrap().clone());
-        let mut first = rdp_alg(&mut first_half, eps)?;
-        let mut second = rdp_alg(&mut second_half, eps)?;
-        first.pop();
-        first.append(&mut second);
-        return Some(first);
-    }
-    let data = vec![data.first()?.to_owned(), data.last()?.to_owned()];
-    return Some(data);
-}
-
-
-fn main() -> Result<(), eframe::Error> {
-    env::set_var("RUST_BACKTRACE", "1");
-    let list : Vec<PlotPoint> = create_sin(0.0, 2.0, 30).iter().map(|x| x.as_plot_point()).collect();
-    // println!("Hello, world!");
-    // println!("{:?}", list);
-    // let range = Range::new(0.0,10.0,10);
-    // let vals : Vec<_> = range.collect();
-    // println!("{:?}", vals);
-    // let mut list : Vec<Point> = create_sin(0.0, 2.0, 30);
-    // println!("{:?}", rdp_alg(&mut list, 0.2));
-
-
-    env_logger::init(); // Log to stderr (if you run with `RUST_LOG=debug`).
-    let options = eframe::NativeOptions::default();
-    eframe::run_native(
-        "Plot",
-        options,
-        Box::new(|_cc| Box::<PlotExample>::default()),
-    )
-}
-
-struct PlotExample {
+pub struct PlotExample {
     lock_x: bool,
     lock_y: bool,
     ctrl_to_zoom: bool,
     shift_to_horizontal: bool,
     zoom_speed: f32,
     scroll_speed: f32,
+    range_start: f64,
+    range_end: f64,
+    range_steps: u32,
+    func: MathFunc,
+    epsilon: f64,
 }
 
 impl Default for PlotExample {
@@ -139,6 +31,11 @@ impl Default for PlotExample {
             shift_to_horizontal: false,
             zoom_speed: 1.0,
             scroll_speed: 1.0,
+            range_start: 0.0,
+            range_end: 1.0,
+            range_steps: 300,
+            func: MathFunc::Sin,
+            epsilon: 0.05,
         }
     }
 }
@@ -165,6 +62,38 @@ impl eframe::App for PlotExample {
                         .speed(0.1),
                 );
                 ui.label("Scroll speed").on_hover_text("How fast to pan with the mouse wheel");
+            });
+            ui.horizontal(|ui| {
+                ui.add(
+                    DragValue::new(&mut self.range_start)
+                        .clamp_range(0.0..=5.0)
+                        .speed(0.1),
+                );
+                ui.label("start value").on_hover_text("TBD");
+            });
+            ui.horizontal(|ui| {
+                ui.add(
+                    DragValue::new(&mut self.range_end)
+                        .clamp_range(0.0..=5.0)
+                        .speed(0.1),
+                );
+                ui.label("end value").on_hover_text("TBD");
+            });
+            ui.horizontal(|ui| {
+                ui.add(
+                    DragValue::new(&mut self.range_steps)
+                        .clamp_range(10..=1000)
+                        .speed(10),
+                );
+                ui.label("steps").on_hover_text("TBD");
+            });
+            ui.horizontal(|ui| {
+                ui.add(
+                    DragValue::new(&mut self.epsilon)
+                        .clamp_range(0.0..=5.0)
+                        .speed(0.001),
+                );
+                ui.label("epsilon").on_hover_text("TBD");
             });
         });
         egui::CentralPanel::default().show(ctx, |ui| {
@@ -226,14 +155,37 @@ impl eframe::App for PlotExample {
                         }
                         plot_ui.translate_bounds(pointer_translate);
                     }
-                    let some_points : Vec<[f64;2]> = create_sin(0.0, 2.0, 300).iter().map(|x| x.as_arr()).collect();
-                    let sine_points = PlotPoints::from(some_points);
+                    let mut some_points : Vec<rdp::Point> = rdp::create_sin(&self.range_start, &self.range_end, &self.range_steps);
+                    let plot_points : Vec<[f64;2]> = some_points.clone().iter().map(|x| x.as_arr()).collect();
+                    let sine_points = PlotPoints::from(plot_points);
                     plot_ui.line(Line::new(sine_points).name("somepoints"));
-                    let mut some_points : Vec<Point> = create_sin(0.0, 2.0, 300);
-                    let some_points : Vec<[f64;2]> = rdp_alg(&mut some_points, 0.05).unwrap().iter().map(|x| x.as_arr()).collect();
+                    // let mut some_points : Vec<Point> = create_sin(0.0, 2.0, 300);
+                    let some_points : Vec<[f64;2]> = rdp::rdp_alg(&mut some_points, &self.epsilon).unwrap().iter().map(|x| x.as_arr()).collect();
                     let sine_points = PlotPoints::from(some_points);
                     plot_ui.line(Line::new(sine_points).name("rdp"));
                 });
         });
     }
+}
+
+
+fn main() -> Result<(), eframe::Error> {
+    // env::set_var("RUST_BACKTRACE", "1");
+    // let list : Vec<PlotPoint> = create_sin(0.0, 2.0, 30).iter().map(|x| x.as_plot_point()).collect();
+    // println!("Hello, world!");
+    // println!("{:?}", list);
+    // let range = Range::new(0.0,10.0,10);
+    // let vals : Vec<_> = range.collect();
+    // println!("{:?}", vals);
+    // let mut list : Vec<Point> = create_sin(0.0, 2.0, 30);
+    // println!("{:?}", rdp_alg(&mut list, 0.2));
+
+
+    env_logger::init(); // Log to stderr (if you run with `RUST_LOG=debug`).
+    let options = eframe::NativeOptions::default();
+    eframe::run_native(
+        "Plot",
+        options,
+        Box::new(|_cc| Box::<PlotExample>::default()),
+    )
 }
---
+++ src/rdp.rs
@@ -0,0 +1,94 @@
+use std::f64::consts::PI;
+
+pub fn create_sin(start : &f64, stop : &f64, steps : &u32) -> Vec<Point> {
+    let mut list : Vec<_>  = Vec::new();
+    let step_size : f64 = (stop - start)/((steps -1) as f64);
+    for i in 0..steps.clone() {
+        list.push(start + (i as f64)*step_size);
+    }
+    let list : Vec<_> = list.iter()
+                        .map(|&x| Point {y: ((x as f64)*2.0*PI).sin(), x})
+                        .collect();
+    list
+}
+
+#[derive(Debug, Clone, Copy)]
+pub struct Point {
+    y : f64,
+    x : f64
+}
+
+impl Point {
+    pub fn as_arr(&self) -> [f64;2] {
+        return [(&self.x).to_owned(),(&self.y).to_owned()];
+    }
+}
+
+pub struct Range {
+    start : f64,
+    end : f64,
+    steps : u32,
+    current : f64,
+}
+
+impl Range {
+    fn new(start : f64, end : f64, steps : u32) -> Range {
+        Range {start:start, end:end, steps:steps, current:start - (end-start)/(steps as f64)}
+    }
+}
+
+impl Iterator for Range {
+    type Item = f64;
+    
+    fn next(&mut self) -> Option<Self::Item> {
+        self.current += (self.end - self.start)/(self.steps as f64);
+        if self.current >= self.end{
+            None
+        } else {
+            Some(self.current)
+        }
+    }
+}
+
+
+// struct regularIntervalpoint
+
+fn perpendicular_distance(l1 : &Point, l2 : &Point, p : &Point) -> f64{
+    let num = ((l2.x-l1.x)*(p.y-l1.y) - (p.x - l1.x)*(l2.y-l1.y)).abs();
+    let den = ((l2.x-l1.x).powi(2) + (l2.y-l1.y).powi(2)).sqrt();
+    num/den
+}
+
+fn vertical_distance(l1 : &Point, l2 : &Point, p : &Point) -> f64{
+    let slope = (l2.y-l1.y)/(l2.x-l1.x);
+    let x_dist = p.x-l1.x;
+    let line_y = l1.y + x_dist*slope;
+    (line_y - p.y).abs()
+}
+
+pub fn rdp_alg(data : &mut Vec<Point>, eps : &f64) -> Option<Vec<Point>> {
+    let mut dmax : f64 = 0.0;
+    let mut imax =  0;
+    let l1 = data.first()?;
+    let l2 = data.last()?;
+    for i in 1..(data.len()-2) {
+        let p = data.get(i)?;
+        let d = vertical_distance(l1, l2, p);
+        if d > dmax {
+            imax = i;
+            dmax = d;
+        }
+    }
+    if dmax > *eps {
+        let mut first_half = data.clone();
+        let mut second_half = first_half.split_off(imax);
+        first_half.push(second_half.first().unwrap().clone());
+        let mut first = rdp_alg(&mut first_half, eps)?;
+        let mut second = rdp_alg(&mut second_half, eps)?;
+        first.pop();
+        first.append(&mut second);
+        return Some(first);
+    }
+    let data = vec![data.first()?.to_owned(), data.last()?.to_owned()];
+    return Some(data);
+}
